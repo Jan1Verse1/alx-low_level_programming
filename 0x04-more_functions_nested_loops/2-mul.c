120417038b84ad3315805584eb1f31f3afc2f2f9
Jan1Verse1 <109815269+Jan1Verse1@users.noreply.github.com>
2022-09-15 21:54:43 +0100

    functions

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,16 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * mul - gives the product of a and b
- * @a: variable
- * @b: variable
- *
- * Return: a * b
+ * mul - multiplies two integers
+ * @a: int to be multiplied to b
+ * @b: int to be multiplied to a
+ * Return: the result of the operation
  */
-
 int mul(int a, int b)
 {
-return (a * b);
+	int c;
+
+	c = a * b;
+
+	return (c);
 }
